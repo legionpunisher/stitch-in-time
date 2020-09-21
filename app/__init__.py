c4485767fe2ff6b3b9b6b3fdcbc0f719a04610e1
legionpunisher <mbuguamoses656@gmail.com>
2020-09-21 23:07:32 +0300

    imported our sqlmodule imported flask imorted modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,7 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
-
-def create_app(config_name):
-    app = Flask(__name__)
-
-    # ....
-
-    # Initializing flask extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
