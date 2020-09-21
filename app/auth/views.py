bdc47621f85d2e5b59677bb94cc3b0530557014a
legionpunisher <mbuguamoses656@gmail.com>
2020-09-21 22:37:30 +0300

    created a main route and a pitch route for the classes.. the render template will return the required details to our html pages

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,55 @@ from ..models import Pitches, User, Comments
 from . import main
 from .. import db,photos
 from .forms import PitchForm,CommentForm, UpdateProfile
+
+
+@main.route('/')
+def index():
+    '''
+    my index page
+    return
+    '''
+    message= "Hello"
+    title= 'Pitch It Ip!'
+    return render_template('index.html', message=message,title=title)
+
+@main.route('/pitch/', methods = ['GET','POST'])
+@login_required
+def new_pitch():
+
+    form = PitchForm()
+
+    if form.validate_on_submit():
+        category = form.category.data
+        pitch= form.pitch.data
+        title=form.title.data
+
+        # Updated pitchinstance
+        new_pitch = Pitches(title=title,category= category,pitch= pitch,user_id=current_user.id)
+
+        title='New Pitch'
+
+        new_pitch.save_pitch()
+
+        return redirect(url_for('main.index'))
+
+    return render_template('pitch.html',pitch_entry= form)
+
+@main.route('/categories/<cate>')
+def category(cate):
+    '''
+    function to return the pitches by category
+    '''
+    category = Pitches.get_pitches(cate)
+    # print(category)
+    title = f'{cate}'
+    return render_template('categories.html',title = title, category = category)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(author = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
