14706eba13c85cef25e230ef1a775c8def9a2726
legionpunisher <mbuguamoses656@gmail.com>
2020-09-21 22:35:51 +0300

    imported the classes user, pitch and comments

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,3 +1,7 @@
 from flask import render_template
 from flask import render_template,request,redirect,url_for
 from flask_login import login_required,current_user
+from ..models import Pitches, User, Comments
+from . import main
+from .. import db,photos
+from .forms import PitchForm,CommentForm, UpdateProfile
